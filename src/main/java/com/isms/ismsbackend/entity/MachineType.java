7358cc0d4935da176be2293eff97787dab30eed1
muran0218 <610688210@qq.com>
2021-10-28 21:04:32 +0800

    lzj:设备模块填写

---
+++ src/main/java/com/isms/ismsbackend/entity/MachineType.java
@@ -0,0 +1,29 @@
+package com.isms.ismsbackend.entity;
+
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * machine_type
+ * @author
+ * 设备类型实体
+ */
+@Data
+public class MachineType implements Serializable {
+    /**
+     * 设备类型
+     */
+    private Integer typeId;
+
+    /**
+     * 设备类型名称
+     */
+    private String typeName;
+
+    /**
+     * 是否删除   0：未删除   1：已删除
+     */
+    private Integer isDelete;
+
+    private static final long serialVersionUID = 1L;
+}
